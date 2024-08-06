ded0297bb02b970111fec439a0e0b2cb0d58f59f
Jayanta66 <jayhalder18@gmail.com>
2024-08-07 00:14:46 +0530

    deploy vite react App on Github pages

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -39,7 +39,7 @@ const Contact = () => {
             <SiGmail className="icons" />
           </a>
           <a href="https://www.fiverr.com/jayanta_halder" target="_blank" className="items">
-            <fa-upwork className="icons" />
+            <FaUpworkSquare className="icons" />
           </a>
         </div>
       </div>
