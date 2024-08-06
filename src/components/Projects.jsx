12be3fc367d984ce174feec15f0a49e5544cb23b
Jayanta66 <jayhalder18@gmail.com>
2024-08-06 19:47:26 +0530

    deploy vite react App on Github pages

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -40,7 +40,7 @@ const Projects = () => {
                     <h5 className="card-title">{data.title}</h5>
                     <p className="card-text">{data.description}</p>
                     <a href={data.demo} className="btn btn-primary mx-3">
-                      Demo
+                      Details
                     </a>
                     <a href={data.source} className="btn btn-warning">
                       Code
