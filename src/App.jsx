e6e27a3520da5e932223e17f21263cebf190d388
Jayanta66 <jayhalder18@gmail.com>
2024-11-20 12:08:21 +0530

    add freelancing site Nov 2024

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ const App = () => {
       <div className="container">
         <Home />
         <Freelancing/>
+        <br></br>
         <Experience />
         <Skills />
         <Projects />
--- src/components/Freelancing.jsx
+++ src/components/Freelancing.jsx
@@ -5,23 +5,26 @@ const Freelancing = () => {
     <>
       <div className="container freelancing" id="freelancing">
         <h1>Freelancing sites</h1>
-        <h1>I am available on these platform also . Just click on the site name to connect with me through these freelancing site. Thank you .</h1>
+        <h5>I am available on these platform also . Just click on the site name to connect with me through these freelancing site. Thank you .</h5>
         <a href="https://www.fiverr.com/s/NNoXk98" className="nav_items">
-            <h1>Fiverr</h1> 
+            <h2>Fiverr</h2> 
           </a>
           <br></br>
           <br></br>
 
-          <a href="https://www.upwork.com/freelancers/~017c81fa775b0f97ec" className="nav_items">
-            <h1>Upwork</h1>
+          <a href="https://www.upwork.com/freelancers/~017c81fa775b0f97ec" className="nav_items" >
+            <h2 color="red">Upwork</h2>
           </a>
           <br></br>
           <br></br>
 
           <a href="https://www.freelancer.in/u/jayhalder555" className="nav_items">
-            <h1>Freelancer</h1>
+            <h2>Freelancer</h2>
           </a>
-        
+          <br></br>
+          <br></br>
+          <br></br>
+
       </div>
     </>
   );
